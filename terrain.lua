964de6786c2bf2f8e7b21a91db24bdc6c5fb7b36
Paul Laffitte <paul.laffitte@epitech.eu>
2019-03-26 22:38:42 +0100

    grass and mountains

--- terrain.lua
+++ terrain.lua
@@ -8,13 +8,23 @@ function terrain:load()
 		grass = love.image.newImageData("assets/grass.png"),
 		sand = love.image.newImageData("assets/sand.png"),
 		snow = love.image.newImageData("assets/snow.png"),
+		stone = love.image.newImageData("assets/stone.png"),
 		water = love.image.newImageData("assets/water.png")
 	}
 end
 
 function terrain:render(x, y)
 	contientAltitude = math.tanh(utils.noise(x, y, 500) * 3 - 1.5) * 1.5
-	altitude = (contientAltitude * utils.noise(x, y, 70) * 200 - 90) * utils.noise(x, y, 30) + 5
+	altitude = (contientAltitude * utils.noise(x, y, 70) * 300 - 90) * utils.noise(x, y, 30) + 5
+	if altitude > 60 then
+		return utils.pixelFromTexture(x, y, self.assets.snow)
+	end
+	if altitude > 30 then
+		return utils.pixelFromTexture(x, y, self.assets.stone)
+	end
+	if altitude > 3 then
+		return utils.pixelFromTexture(x, y, self.assets.grass)
+	end
 	if altitude > 0 then
 		return utils.pixelFromTexture(x, y, self.assets.sand)
 	end
